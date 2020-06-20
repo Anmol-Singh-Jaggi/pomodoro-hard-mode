8af5ce4851920290820694e25f65bdc2993b7ea3
Anmol Singh Jaggi <ajaggi@linkedin.com>
2020-06-20 22:17:25 +0530

    Prevent main loop from starting twice due to race condition

--- pomodoro-hard-mode/src/screen_state_observer.py
+++ pomodoro-hard-mode/src/screen_state_observer.py
@@ -2,6 +2,7 @@
 import multiprocessing as mp
 import subprocess
 import logging
+import threading
 logging.basicConfig(format='%(levelname)s:%(process)d:%(asctime)s:::%(message)s', datefmt='%d-%b-%y_%H:%M:%S', level=logging.DEBUG)
 import Foundation
 import AppKit
@@ -21,14 +22,20 @@ class ScreenStateObserver(AppKit.NSObject):
         pkill('pomodoro_statusbar')
 
     def screenOnHandler_(self, _):
-        logging.debug('Starting main loop again')
-        cmd = "ps -ef | grep 'pomodoro_main_loop' | grep -v grep | awk '{print $2}'"
-        res = subprocess.run(cmd, shell=True, capture_output=True)
-        if len(res.stdout) > 0:
-            logging.warn('Main loop already running!!', file=sys.stderr)
-            return
-        main_loop_process = mp.Process(name='pomodoro_main_loop', target=main_loop)
-        main_loop_process.start()
+        # Semaphore is required because this method is sometimes being called
+        # twice in parallel when the screen unlocks.
+        # This can cause 2 main loop processes to start at the same time due to
+        # race conditions.
+        # Dont know why this is happening; seems like a bug in OSX libraries.
+        with self.screen_on_handler_semaphore:
+            logging.debug('Starting main loop again')
+            cmd = "ps -ef | grep 'pomodoro_main_loop' | grep -v grep | awk '{print $2}'"
+            res = subprocess.run(cmd, shell=True, capture_output=True)
+            if len(res.stdout) > 0:
+                logging.error('Main loop already running!!')
+                return
+            main_loop_process = mp.Process(name='pomodoro_main_loop', target=main_loop)
+            main_loop_process.start()
 
 
 def start_screen_state_observer():
@@ -41,6 +48,7 @@ def start_screen_state_observer():
     setproctitle.setproctitle(mp.current_process().name)
     nc = Foundation.NSDistributedNotificationCenter.defaultCenter()
     screen_state_observer = ScreenStateObserver.new()
+    screen_state_observer.screen_on_handler_semaphore = threading.Semaphore()
     nc.addObserver_selector_name_object_(screen_state_observer, 'screenOffHandler:', 'com.apple.screenIsLocked', None)
     nc.addObserver_selector_name_object_(screen_state_observer, 'screenOnHandler:', 'com.apple.screenIsUnlocked', None)
     AppHelper.runConsoleEventLoop()
